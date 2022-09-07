4284f8cad5776e0fdf51519fcdbcbf063912ff3a
Your Name <samsulr99@gmail.com>
2022-09-07 10:52:08 +0700

    List of Map

--- App.js
+++ App.js
@@ -3,13 +3,13 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
-import Form from "./src/screens/Form";
+import Map from "./src/screens/Map";
 
 export default function App() {
   return (
     <View style={{ marginTop: 100 }}>
       <StatusBar />
-      <Form />
+      <Map />
     </View>
   );
 }
