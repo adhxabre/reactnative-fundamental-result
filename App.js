045fe2ee10bfa9275128cf9f0a93e3f383e2eb49
Your Name <samsulr99@gmail.com>
2022-09-07 09:32:54 +0700

    Event

--- App.js
+++ App.js
@@ -3,14 +3,13 @@ import React from "react";
 import { View } from "react-native";
 
 //Import Screen
-import EmbedExpression from "./src/screens/EmbedExpression";
+import Event from "./src/screens/Event";
 
 export default function App() {
   return (
     <View style={{ marginTop: 200 }}>
       <StatusBar />
-      {/* Use Component */}
-      <EmbedExpression />
+      <Event />
     </View>
   );
 }
