f047c07acaef51daa6a4d2306deba66cec091005
Your Name <samsulr99@gmail.com>
2022-09-07 15:34:40 +0700

    native base

---
+++ Container.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Text, Box } from "native-base";
+
+// Import Screen
+import FormNativeBase from "./src/screens/FormNativeBase";
+import Hello from "./src/screens/Hello";
+
+export default function Container() {
+  return <FormNativeBase />;
+}
